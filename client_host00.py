89c82884ac1d78abc36fc29ff53e23ca090b3717
John Linthicum <john.linthicum@stud.h-da.de>
2023-11-13 16:24:08 +0100

    added UDP python scripts

---
+++ client_host00.py
@@ -0,0 +1,23 @@
+import socket
+import time
+
+# Client settings
+UDP_IP = "router00"  # Replace with the actual IP of router00
+UDP_PORT = 55055
+
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    # Send a request to the server
+    client_socket.sendto(b"Request", (UDP_IP, UDP_PORT))
+
+    # Receive the result from the server
+    data, addr = client_socket.recvfrom(1024)
+    result = float(data.decode())
+
+    # Print the received result
+    print(f"Received result {result} from {addr} at {time.time()}")
+
+    # Sleep for some time before sending the next request
+    time.sleep(1)
---
+++ server_router00.py
@@ -0,0 +1,28 @@
+import socket
+import time
+
+# Server settings
+UDP_IP = "0.0.0.0"
+UDP_PORT = 55055
+
+# Create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to a specific address and port
+server_socket.bind((UDP_IP, UDP_PORT))
+
+print(f"Server started on {UDP_IP}:{UDP_PORT}")
+
+while True:
+    # Receive data from the client
+    data, addr = server_socket.recvfrom(1024)
+
+    # Process the request (compute result of the function)
+    result = time.time()  # Replace this with your actual function
+
+    # Print server information
+    print(f"Received request from {addr} at {time.time()}")
+    print(f"Sending result {result} to {addr} at {time.time()}")
+
+    # Send the result back to the client
+    server_socket.sendto(str(result).encode(), addr)
